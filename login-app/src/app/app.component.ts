cd7983ad3a474f1469bb2bb7fabcf14b1b089935
WhatIsOO <miguel@suitebox.com>
2017-08-28 22:00:09 +1200

    Sending over empty request to a dummy end point

--- login-app/src/app/app.component.ts
+++ login-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  username = '';
+  password = '';
+  url = 'https://fbr50l67ie.execute-api.us-east-1.amazonaws.com/jwBooking'
+  constructor(private http: Http) { }
+
+  login(): Promise<boolean> {
+    return this.http.post(this.url, null)
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
+  }
+
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
