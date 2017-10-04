36390e27d39ca44df06f60c8b0a9b3afa619dbbf
WhatIsOO <miguel@suitebox.com>
2017-10-04 17:44:42 +1300

    cleaned up code

--- login-app/src/app/app.component.ts
+++ login-app/src/app/app.component.ts
@@ -9,45 +9,43 @@ import { LoginService } from './services/login.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loginService : LoginService;
+export class AppComponent implements OnInit {
+  loginService: LoginService;
+    // Login States
+    loginState: String;
+    LOGIN_STATES: object;
 
-  constructor(loginService: LoginService) {
-    this.loginService = loginService;
+    // Login var
+    password: String;
+    username: String;
+
+    // Signin var
+    sPassword: String;
+    sConfPassword: String;
+    sUsername: String;
+    sEmail: String;
+    sConfEmail: String;
+
+    // Comfirm var
+    confirmCode: String;
+
+  constructor() {
   }
 
   ngOnInit() {
     this.onInitStatePage();
   }
-  
-  // Login States
-  loginState: String;
-  LOGIN_STATES: object;
-
-  // Login var
-  password: String;
-  username: String;
-
-  // Signin var
-  sPassword: String;
-  sConfPassword: String;
-  sUsername: String;
-  sEmail: String;
-  sConfEmail: String;
-
-  // Comfirm var
-  confirmCode: String;
 
   /*****************************************
    * Login / Signin / Confirm Action buttons
    *****************************************/
-  
+
   onLoginButtonClicked(): void  {
     console.log('Login', this.username, this.password);
-    this.loginService.login(this.username, this.password, 
+    this.loginService.login(this.username, this.password,
       function (response) {
         console.log('Success', response);
-        // Storing current installed user details      
+        // Storing current installed user details
         localStorage.setItem('currentUser', JSON.stringify(response));
         // window.location.href = "https://google.com";
      }, function (response) {
@@ -57,21 +55,22 @@ export class AppComponent {
   }
 
   onSignUpButtonClicked(): void {
-    console.log('Sign Up', this.sUsername, this.sPassword, this.sConfPassword, 
+    console.log('Sign Up', this.sUsername, this.sPassword, this.sConfPassword,
       this.sEmail, this.sConfEmail);
   }
 
   onSignUpConfirmButtonClicked(): void {
-    console.log('Sign Up Comfirm', this.confirmCode, this.sUsername, 
+    console.log('Sign Up Comfirm', this.confirmCode, this.sUsername,
     this.sPassword, this.sConfPassword, this.sEmail, this.sConfEmail);
   }
 
   /***************************
    * Page transition functions
    ***************************/
-  
+
   onInitStatePage(): void {
     // Get param in the address bar
+    console.log('asdasdasd');
   }
 
   onSwitchToSignup(): void {
@@ -83,7 +82,7 @@ export class AppComponent {
     // Change the address in the bar
     // Change the page state var
   }
-  
+
   onSwitchToLogin(): void {
     // Change the address in the bar
     // Change the page state var
